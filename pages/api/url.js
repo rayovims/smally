4018175e5fb57e25fd39046e3eac0764ca45112d
ojedarm <ojedarm@pella.com>
2022-01-09 00:22:24 -0500

    added logic to handle same url shortend

--- pages/api/url.js
+++ pages/api/url.js
@@ -1,5 +1,22 @@
 const { connectToDatabase } = require('../../lib/mongodb');
 
+const sendResp = (id) => {
+    let _url = "";
+    if(process.env.NODE_ENV === "development") {
+        _url = "http://localhost:3000/" + id;
+    } else {
+        _url = "https://smally.vercel.app/" + id;
+    }
+    const obj = {
+        response: {
+            status: 200,
+            message: "url successfully shortened",
+            value: _url
+        }
+    }
+    return obj;
+}
+
 export default async function handler(req, res) {
     const url = req.body.url;
     if(!url) {
@@ -8,20 +25,18 @@ export default async function handler(req, res) {
 
     const { db } = await connectToDatabase();
 
+    await db.collection("urls").find({ url }).toArray((err, resp) => {
+        if(resp.length) {
+            const obj = sendResp(resp[0].ref);
+            res.status(200).json(obj);
+        }
+    })
+
     await db.collection("urls").find({}).toArray(function(err, resp) {
         let id = resp[resp.length - 1].ref;
         db.collection("urls").insertOne({ ref: id + 1, url });
         let value = id + 1;
-        let _url = "";
-        if(process.env.NODE_ENV === "development") {
-            _url = "http://localhost:3000/" + value;
-        } else {
-            _url = "https://smally.vercel.app/" + value;
-        }
-        res.status(200).json({ response: {
-            status: 200,
-            message: "url successfully shortened",
-            value: _url
-        }});
+        const obj = sendResp(value);
+        res.status(200).json(obj);
     });
 }
